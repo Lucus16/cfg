cceb7c68d83fa741b290bd22836e96b58062a330
Lars Jellema <lars.jellema@gmail.com>
2022-06-15 15:51:01 +0200

    st: Add boxdraw and window title stack

--- pkgs/st.nix
+++ pkgs/st.nix
@@ -5,6 +5,6 @@
     owner = "Lucus16";
     repo = "st";
     rev = "master";
-    sha256 = "0xwz9bi3v1ww82bf12vl0fr2dz8synnjiabc166zra0jhhg4f1b7";
+    sha256 = "1lnxx99b9r01f8fc7qg44bvk0k9rrb97yx7dkys58m5ss9ax6jv4";
   };
 }))
