9d0ca5ef9d9aea7ef7229e80348bd0d989dfd12a
Lars Jellema <lars.jellema@gmail.com>
2022-10-07 13:48:53 +0200

    tisdone: Fix delay before exit

--- pkgs/tisdone.nix
+++ pkgs/tisdone.nix
@@ -2,9 +2,9 @@
 
 writeShellScriptBin "tisdone" ''
   if "$@"; then
-    ${alsa-utils}/bin/aplay -q ${../res/ok.wav} 2>/dev/null & exit 0
+    ${alsa-utils}/bin/aplay ${../res/ok.wav} >/dev/null 2>/dev/null & exit 0
   else
     rv=$?
-    ${alsa-utils}/bin/aplay -q ${../res/error.wav} 2>/dev/null & exit $rv
+    ${alsa-utils}/bin/aplay ${../res/error.wav} >/dev/null 2>/dev/null & exit $rv
   fi
 ''
