340a2910d2f8d1b495db6c520a144a9a1182a205
Lars Jellema <lars.jellema@gmail.com>
2023-12-07 23:15:28 +0100

    relto: Add peer for nokia6

--- dev/relto.nix
+++ dev/relto.nix
@@ -108,6 +108,8 @@ in {
       privateKeyFile = "/etc/wireguard/larsnet.secret";
       postSetup = "ip link set mtu 1360 dev larsnet";
       peers = lib.attrValues {
+        nokia6.allowedIPs = [ "172.27.0.2" ];
+        nokia6.publicKey = "LQI1iPdhSk5QxrTsL+2ZLRICz9wD4qIJZjrfc7w4NkE=";
         amateria.allowedIPs = [ "172.27.0.4" ];
         amateria.publicKey = "lmL6nSdE3eP3R7KwHi7N4+Iaj2k6Qh9rNWigxYgD8CI=";
         edanna.allowedIPs = [ "172.27.0.5" ];
